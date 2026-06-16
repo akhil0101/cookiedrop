cb6f64af874f3c4de4e3a5d529e5338a5db358c2
Akhil Kandakatla <akhilkandakatla1997@gmail.com>
2026-06-16 17:56:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 (function dropCookies() {
-  // 1. A pool of delicious cookie names
-  const cookies = [
     "pixel_v4_engaged",
     "free_shipping_eligible",
     "no_promo_active",
     "true_active",
-    "Peanuttech_enthusiastsButter",
+    "organic_search_google",
   ];
-
-  // 3. Drop the session cookies (No expires attribute means they die when the browser closes)
   cookies.forEach((cookie, index) => {
     document.cookie = `__clarity_cookie_${index + 1}=${cookie};SameSite=Lax`;
   });
